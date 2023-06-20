b931b0571e64165024b99105f0e61ebe8ac9f90c
itzrizvi <shahriar.me.work@gmail.com>
2023-06-20 21:44:02 +0600

    Code Cleanup

--- src/middlewares/onReqTokenGenerator.js
+++ src/middlewares/onReqTokenGenerator.js
@@ -22,8 +22,6 @@ const onReqTokenGenerate = async (req, res, next) => {
         return next();
     }
 
-    console.log(decodedToken);
-
     let authUser;
     try {
         authUser = await db.User.findOne({ _id: decodedToken.id });
